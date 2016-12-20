b5fba3df18d3d65e7cf4a5cd51281271a484f06c
natalypazm <1000104644.rr@gmail.com>
2016-12-20 10:31:28 -0500

    promedio de tres numeros

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ejercicio-2
+
+Inicio: Promedio de tres números
+
+## Ingreso de datos
+-número1
+-número2
+-número3
+-resultado
+
+# Proceso
+-pedir número1
+-pedir número2
+-pedir número2
+-sumar número1, número2, número3,dividir entre 3 y almacenar resultado
+-mostrar resultado
+
+*fin*
---
+++ main.js
@@ -0,0 +1,6 @@
+var numero1,numero2, numero3, resultado;
+número1 = prompt("Ingrese el primer número");
+número2 = prompt("Ingrese el segundo número");
+número3 = prompt("Ingrese el tercer número");
+resultado = (parseFloat(número1) + parseFloat(número2) + parseFloat(número3))/3;
+alert(resultado);
